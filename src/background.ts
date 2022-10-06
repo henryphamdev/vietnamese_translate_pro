f3ec68a894be6952ed67353643c5fe9270e09dca
henryphamdev <phamvanthanhdeveloper@gmail.com>
2022-10-07 00:38:31 +0700

    update

--- src/background.ts
+++ src/background.ts
@@ -5,31 +5,54 @@ function polling() {
 
 polling();
 
-function searchTerapeak(word:any){
-  var source = word.selectionText;
-  // chrome.tabs.create({url: `https://translate.google.com/?sl=en&tl=vi&text=${query}&op=translate`});
-  console.log(`Translated keyword "${source}"`)  
-  chrome.tabs.query({active: true, currentWindow: true}, async (tabs:any) => {
-    let translatedRaw:any = await TEST_HTTPREQUEST(source)
-    let translated = ParseDataTranslateFromGG(translatedRaw)
-    chrome.tabs.sendMessage(tabs[0].id, {action: "send_translated_value", value : translated});
-    console.log(`Source text: ${source} meaning ${translated}`)
-    chrome.storage.sync.get("translated_history",
-    function(data:any) {
-      if (!data?.translated_history){
-        data.translated_history = [{source, translated}]
-      }else{
-        data.translated_history.unshift({source, translated})
+function searchTerapeak(contextMenu:any){
+
+  var source = contextMenu.selectionText;
+  if (contextMenu.menuItemId == "translate") {
+    console.log(`Translated keyword "${source}"`)  
+    chrome.tabs.query({active: true, currentWindow: true}, async (tabs:any) => {
+      let translatedRaw:any = await TEST_HTTPREQUEST(source)
+      let translated = ParseDataTranslateFromGG(translatedRaw)
+      chrome.tabs.sendMessage(tabs[0].id, {action: "send_translated_value", value : translated});
+      console.log(`Source text: ${source} meaning ${translated}`)
+      chrome.storage.sync.get("translated_history",
+      function(data:any) {
+        if (!data?.translated_history){
+          data.translated_history = [{source, translated}]
+        }else{
+          data.translated_history.unshift({source, translated})
+        }
+        console.log(data)
+        chrome.storage.sync.set({
+          translated_history: data.translated_history
+        }, function() {
+            console.log("Save translated history successful!");
+        });
       }
-      console.log(data)
-      chrome.storage.sync.set({
-        translated_history: data.translated_history
-      }, function() {
-          console.log("Save translated history successful!");
-      });
-    }
-  )}
-)};
+    )})
+  }
+  if (contextMenu.menuItemId == "reminder") {
+    console.log(`Translated keyword "${source}"`)  
+    chrome.tabs.query({active: true, currentWindow: true}, async (tabs:any) => {
+      chrome.tabs.sendMessage(tabs[0].id, {action: "reminder", value : source});
+      chrome.storage.sync.get("reminder",
+      function(data:any) {
+        if (!data?.reminder){
+          data.reminder = [{source}]
+        }else{
+          data.reminder.unshift({source})
+        }
+        console.log(data)
+        chrome.storage.sync.set({
+          reminder: data.reminder
+        }, function() {
+            console.log("Save translated history successful!");
+        });
+      }
+    )})
+  }
+  // chrome.tabs.create({url: `https://translate.google.com/?sl=en&tl=vi&text=${query}&op=translate`});
+};
 
 async function TEST_HTTPREQUEST(sourceString:string) {
   return new Promise((resolve, reject) => {
@@ -71,10 +94,17 @@ function ParseDataTranslateFromGG(raw:string){
 
 chrome.contextMenus.removeAll(function() {
   chrome.contextMenus.create({
-   id: "1",
+   id: "translate",
    title: "Translate to Vi",
    contexts:["selection"],  // ContextType
-  }); })
+  });
+
+  chrome.contextMenus.create({
+    id: "reminder",
+    title: "Add to Reminder",
+    contexts:["selection"],  // ContextType
+  })
+})
 
 chrome.contextMenus.onClicked.addListener(searchTerapeak);
 
@@ -99,8 +129,33 @@ const start = () => {
   })
 }
 
+const reminder = () => {
+  console.log('go here')
+    chrome.tabs.query({active: true, currentWindow: true}, async (tabs:any) => {
+      chrome.storage.sync.get("reminder",
+      async function(data:any) {
+        if (data?.reminder){
+          let ask = data.reminder[Math.floor(Math.random()*data?.reminder.length)]
+          if (!tabs.length ){
+            console.log(`Cannot get active tab`)
+          }else{
+            chrome.tabs.sendMessage(tabs[0].id, {action: "reminder", value : ask.source}, (res:any) => {
+              console.log(`Response from reciever: ${res}`)
+            });
+          }
+        }
+      }
+    )
+  })
+}
+
 
 chrome.alarms.create("remind_english_translated", {
+  delayInMinutes: 3,
+  periodInMinutes: 3
+});
+
+chrome.alarms.create("reminder", {
   delayInMinutes: 5,
   periodInMinutes: 5
 });
@@ -109,6 +164,9 @@ chrome.alarms.create("remind_english_translated", {
 chrome.alarms.onAlarm.addListener(function(alarm) {
   if (alarm.name === "remind_english_translated") {
     start();
+  }
+  if (alarm.name === "reminder") {
+    reminder();
   }
 });
 // setInterval(start, 5 * 60 * 1000);
